1470476ee113cac9fc75fb55be74ce49b43b6b16
Styop <stepan00999@gmail.com>
2024-06-26 11:51:49 +0400

    Homework_topic_03

--- Session_3/module_3.py
+++ Session_3/module_3.py
@@ -1,19 +1,30 @@
-# import time
+import time
 from typing import List
 
 Matrix = List[List[int]]
 
 
 def task_1(exp: int):
-    pass
+    def power_factory(base):
+        return base**exp
+
+    return power_factory
 
 
 def task_2(*args, **kwags):
-    pass
+    for i in args:
+        print(i)
+    for j in kwags.values():
+        print(j)
 
 
 def helper(func):
-    pass
+    def wrapper(*args, **kwargs):
+        print("Hi, friend! What's your name?")
+        func(*args, **kwargs)
+        print("See you soon!")
+
+    return wrapper
 
 
 @helper
@@ -22,7 +33,14 @@ def task_3(name: str):
 
 
 def timer(func):
-    pass
+    def wrapper():
+        start = time.time()
+        func()
+        end = time.time()
+        run_time = end - start
+        print(f"Finished {func.__name__} in {run_time:.4f} secs")
+
+    return wrapper
 
 
 @timer
@@ -31,8 +49,32 @@ def task_4():
 
 
 def task_5(matrix: Matrix) -> Matrix:
-    pass
+    row = len(matrix)
+    col = len(matrix[0])
+    result = [[0] * row for _ in range(col)]
+    print(result)
+    for i in range(col):
+        for j in range(row):
+            result[i][j] = matrix[j][i]
+            print(result)
+
+    return result
+
+
+task_5([[1, 2, 3], [5, 8, 9]])
 
 
 def task_6(queue: str):
-    pass
+    chars = []
+
+    for char in queue:
+        if char == "(":
+            chars.append(char)
+        elif char == ")":
+            if not chars:
+                return False
+            chars = chars[:-1]
+    if len(chars) == 0:
+        return True
+    else:
+        return False
